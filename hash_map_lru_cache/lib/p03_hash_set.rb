2ba3041a4c9009ef44d1f0981df4d75de2a88f3c
Victor Lin <swissashley@hotmail.com>
2016-09-23 10:59:32 -0700

    p03 hashset is completed

--- hash_map_lru_cache/lib/p03_hash_set.rb
+++ hash_map_lru_cache/lib/p03_hash_set.rb
@@ -9,18 +9,27 @@ class HashSet
   end
 
   def insert(key)
+    resize! if num_buckets == @count
+    self[key].push(key) unless include?(key)
+    @count += 1
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    if include?(key)
+      self[key].delete(key)
+      @count -= 1
+    end
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num.hash % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +37,12 @@ class HashSet
   end
 
   def resize!
+    new_store = Array.new(num_buckets * 2) { Array.new }
+    @store.each do |bucket|
+      bucket.each do |el|
+        new_store[el.hash % new_store.length].push(el)
+      end
+    end
+    @store = new_store
   end
 end
