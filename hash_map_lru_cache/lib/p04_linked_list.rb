6b787e443c78f3b237c2e6c910e4960f423deb98
Victor Lin <swissashley@hotmail.com>
2016-09-23 12:49:39 -0700

    linkedlist is working

--- hash_map_lru_cache/lib/p04_linked_list.rb
+++ hash_map_lru_cache/lib/p04_linked_list.rb
@@ -11,10 +11,16 @@ class Link
   def to_s
     "#{@key}: #{@val}"
   end
+
 end
 
 class LinkedList
+  include Enumerable
   def initialize
+    @head = Link.new
+    @tail = Link.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -23,31 +29,73 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    link = @head
+    while link != @tail
+      return link.val if link.key == key
+      link = link.next
+    end
+    nil
   end
 
   def include?(key)
+    link = @head
+    while link.next != @tail
+      return true if link.key == key
+      link = link.next
+    end
+    false
   end
 
   def insert(key, val)
+    new_link = Link.new(key,val)
+    last = @tail.prev
+    last.next = new_link
+    new_link.prev = last
+    new_link.next = @tail
+    @tail.prev = new_link
   end
 
   def remove(key)
+    if include?(key)
+      link = @head
+      while link != @tail
+        if link.key == key
+          break
+        end
+        link = link.next
+      end
+      unless link == @tail
+        last_link = link.prev
+        next_link = link.next
+        last_link.next = next_link
+        next_link.prev = last_link
+      end
+    end
+    nil
   end
 
   def each
+    link = @head.next
+    while link != @tail
+      proc.call(link)
+      link = link.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
