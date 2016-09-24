97b67339a854f27db0f2d8b9ac7d4c2590a458d7
Victor Lin <swissashley@hotmail.com>
2016-09-23 17:06:15 -0700

    lru cache completed

--- hash_map_lru_cache/lib/p06_lru_cache.rb
+++ hash_map_lru_cache/lib/p06_lru_cache.rb
@@ -15,6 +15,14 @@ class LRUCache
   end
 
   def get(key)
+    link = @map[key]
+    unless link.nil?
+      update_link!(link)
+      return link.val
+    else
+      set!(key)
+    end
+
   end
 
   def to_s
@@ -23,14 +31,26 @@ class LRUCache
 
   private
 
-  def calc!(key)
+  def set!(key)
     # suggested helper method; insert an (un-cached) key
+    val = @prc.call(key)
+    @store.insert(key, val)
+    @map[key] = @store.last
+    eject! if count > @max
+    return val
   end
 
   def update_link!(link)
     # suggested helper method; move a link to the end of the list
+    link.prev.next = link.next
+    link.next.prev = link.prev
+    @store.insert(link.key, link.val)
   end
 
   def eject!
+    key = @store.first.key
+    @store.remove(key)
+    @map.delete(key)
+    nil
   end
 end
