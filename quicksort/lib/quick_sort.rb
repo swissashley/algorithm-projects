88bf3761a5a0baa74e8221099be5f9d51ddaee89
Victor Lin <swissashley@hotmail.com>
2016-09-27 18:33:17 -0700

    quicksort is completed

--- quicksort/lib/quick_sort.rb
+++ quicksort/lib/quick_sort.rb
@@ -4,17 +4,24 @@ class QuickSort
 
   # Not in-place. Uses O(n) memory.
   def self.sort1(array)
+    return array if array.length < 2
     pivot = array.first
     left = []
     right = []
     left = array.select {|el| el < pivot}
     right = array.select {|el| el > pivot}
-    self.class.sort1(left) + [pivot] + self.class.sort1(right)
+    sort1(left) + [pivot] + sort1(right)
   end
 
   # In-place.
   def self.sort2!(array, start = 0, length = array.length, &prc)
-    
+    prc ||= Proc.new {|el1, el2| el1 <=> el2}
+    return array if length < 2
+
+    pivot_idx = partition(array, start, length, &prc)
+    sort2!(array, start, pivot_idx - start, &prc)
+    sort2!(array, pivot_idx + 1, length - pivot_idx - 1 + start, &prc)
+    array
   end
 
   def self.partition(array, start, length, &prc)
