2df43a9d701f94831d93fe8a070034c65a3933fc
Victor Lin <swissashley@hotmail.com>
2016-11-18 13:08:09 -0800

    Added Graph

---
+++ Graph/lib/graph.rb
@@ -0,0 +1,25 @@
+class Vertex
+  attr_accessor :value, :in_edges, :out_edges
+  def initialize(value)
+    @value = value
+    @in_edges = []
+    @out_edges = []
+  end
+end
+
+class Edge
+  attr_accessor :from_vertex, :to_vertex, :cost
+  def initialize(from_vertex, to_vertex, cost = 1)
+    @from_vertex = from_vertex
+    @to_vertex = to_vertex
+    @from_vertex.out_edges << this
+    @to_vertex.in_edges << this
+  end
+
+  def destroy!
+    @from_vertex.out_edges.delete(this)
+    @to_vertex.in_edges.delete(this)
+    @from_vertex = nil
+    @to_vertex = nil
+  end
+end
